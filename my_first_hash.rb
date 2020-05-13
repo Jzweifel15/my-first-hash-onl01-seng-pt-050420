ab07d7829aabda75c4c156555a5992704f2205f3
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-13 18:30:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,6 +13,9 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
+  the_manifest = {"whale bone corsets" => 5, 
+                  "porelain vases" => 2,
+                  "oil paintings" => 3}
 
 end
 
@@ -24,6 +27,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["porcelain vases"]
 
 end
 
